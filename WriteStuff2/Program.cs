90d05173f2a56843bda9523b762a5a2506394851
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-06-02 17:59:53 +0100

    Program writes directly in file specified, myfile.txt reset for testing

--- WriteStuff2/Program.cs
+++ WriteStuff2/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
-namespace WriteStuff2
+namespace WriteStuff1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StreamWriter sw = new StreamWriter(args[0]);
+            
+            string phrase = "start";
+
+            while (phrase != "")
+            {
+                phrase = Console.ReadLine();
+                sw.WriteLine(phrase);
+            }
+
+            sw.Close();
         }
     }
 }
