3217ae53353174e207da18f7af88feab8e2452dd
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-06-02 17:52:02 +0100

    Program writes all strings inputed by user into file specified by first argument

--- WriteStuff1/Program.cs
+++ WriteStuff1/Program.cs
@@ -11,11 +11,13 @@ namespace WriteStuff1
             Queue<string> strings = new Queue<string>();
             string phrase = "start";
 
-            while (phrase != null)
+            while (phrase != "")
             {
                 phrase = Console.ReadLine();
                 strings.Enqueue(phrase);
             }
+
+            File.WriteAllLines(args[0], strings);
         }
     }
 }
