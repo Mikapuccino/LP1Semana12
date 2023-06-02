59b8fe3e5eab99c95288f8a19555e10d035f3f92
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-06-02 17:07:41 +0100

    Replace Program class, add Guarda3 class

---
+++ MyEnumerable/Guarda3.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEnumerable
+{
+    public class Guarda3<T>
+    {
+        private T t1, t2, t3;
+
+        public Guarda3()
+        {
+            t1 = default;
+            t2 = default;
+            t3 = default;
+        }
+
+        public T GetItem(int i)
+        {
+            if (i == 0) return t1;
+            if (i == 1) return t2;
+            if (i == 2) return t3;
+            else return default;
+        }
+
+        public void SetItem(int i, T item)
+        {
+            if (i == 0) t1 = item;
+            if (i == 1) t2 = item;
+            if (i == 2) t3 = item;
+            if ((i < 0) || (i > 2)) throw new IndexOutOfRangeException();
+        }
+    }
+}
